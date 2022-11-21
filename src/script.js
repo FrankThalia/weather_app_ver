560e7199e8a0a1a7f7a46ed5b08b88ee5dda0b9c
FrankThalia <116189047+FrankThalia@users.noreply.github.com>
2022-11-21 18:01:43 +0000

    delete console.log, #date data from html

--- src/script.js
+++ src/script.js
@@ -22,7 +22,6 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
@@ -39,6 +38,7 @@ function displayTemperature(response) {
 }
 
 let apiKey = "7d478f69e1b2f5d563653f13f5f91d76";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let city = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
